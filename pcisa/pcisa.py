be9fe2c438889dd252878754d63948be2ab4931c
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-22 14:11:32 -0700

    added pcisa.py skeleton

--- pcisa/pcisa.py
+++ pcisa/pcisa.py
@@ -1,7 +1,52 @@
-import argparse
+# pcisa/pcisa.py
+
 import os
-import sys
 
 import numpy
 import anndata
 import pandas
+
+def run_pca(data: str, n_pcs: int, output: str = None):
+    # Load data and perform preprocessing
+    adata = anndata.read(data)
+    df = pandas.DataFrame(adata.X)
+    
+    # TODO: add preprocessing based on user input
+
+    # Run PCA calculation
+    pca = pca_calculation(df, n_pcs)
+
+    # Save results to .csv
+    if output is None:
+        base = os.path.splitext(data)
+        output = f'{base}_pca.csv'
+    pca.to_csv(output)
+    print(f'PCA results saved to {output}')
+
+def pca_calculation(data: pandas.DataFrame, n_pcs: int):
+    ## TODO: implement PCA calculation, output "pca" variable in pandas DataFrame format
+
+    # center datapoints:
+
+
+    # calculate covariance matrix:
+
+
+    # calculate eigenvectors and eigenvalues:
+
+
+    # sort eigenvectors by eigenvalues:
+
+
+    # select top n_pcs eigenvectors:
+
+
+    # project data onto eigenvectors:
+
+
+    # return projected data:
+    
+
+    principalComponents = pca.fit_transform(data)
+    pcsDF = pandas.DataFrame(data = principalComponents)
+    return pcsDF
