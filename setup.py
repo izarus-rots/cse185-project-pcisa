4af0557fe9f78bde56e19f3919960ea21656804a
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-22 23:05:33 -0700

    minor adjust setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Isabella Garcia',
     author_email='i4garcia@ucsd.edu',
     packages=find_packages(),
-    install_requires=['numpy', 'matplotlib', 'pandas'],
+    install_requires=['numpy', 'anndata', 'matplotlib', 'pandas'],
     entry_points={
         'console_scripts': [
             'pcisa=pcisa.__main__:main'
