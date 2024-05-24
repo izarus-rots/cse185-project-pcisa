025d0f040be0e859f865d7223d73c5afaa42efed
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-24 15:44:13 -0700

    add version tracking

--- setup.py
+++ setup.py
@@ -5,6 +5,21 @@ from setuptools import setup, find_packages
 
 VERSION = '0.1dev' # TODO: implement version tracking
 
+
+# version-keeping code based on pybedtools & gymreklab demo project (https://github.com/gymreklab/cse185-demo-project/tree/main)
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'pcisa/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
+
 setup(
     name='pcisa',
     version=VERSION,
@@ -15,7 +30,7 @@ setup(
     install_requires=['numpy', 'anndata', 'matplotlib', 'pandas'],
     entry_points={
         'console_scripts': [
-            'pcisa=pcisa.__main__:main'
+            'pcisa=pcisa.pcisa:main'
         ]
     },
 )
