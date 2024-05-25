35912ef48fd422acb699b8187d3b50e2cf441721
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-24 18:06:34 -0700

    fixing argument parsing in main

--- pcisa/pcisa.py
+++ pcisa/pcisa.py
@@ -9,18 +9,22 @@ import pandas as pd
 import argparse
 
 def main():
-    parser = argparse.ArgumentParser(description='Principal Component Analysis in Python')
-    parser.add_argument('--data', type=str, required=True, help='Path to input data file, expects matrix-like')
-    parser.add_argument('--n_pcs', type=int, required=True, help='Number of principal components calculated')
+    parser = argparse.ArgumentParser(
+        prog="pcisa",
+        description='Principal Component Analysis in Python'
+    )
+    parser.add_argument('--data', type=str, required=True, help='Path to input data file, expects matrix-like', metavar="data")
+    parser.add_argument('--n_pcs', type=int, required=True, help='Number of principal components calculated', metavar="n_pcs")
 
     parser.add_argument('--standardize', action='store_true', help='Standardize the input data before running PCA')
     parser.add_argument('--output', type=str, help='Path to output file, .csv format default')
     parser.add_argument('--plot', action='store_true', help='Plot the PCA results')
+
     args = parser.parse_args()
 
     print(f'Running PCA on {args.input} and saving results to {args.output}')
 
-    run_pca(args.input, args.output)
+    run_pca(args.data, args.n_pcs)
 
 def run_pca(data: str, n_pcs: int, output: str = None):
     ## TODO: add proper documentation
