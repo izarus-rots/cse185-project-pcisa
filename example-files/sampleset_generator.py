491cf33b221880f9cdbf973d04d10637a9ff3957
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-24 18:20:21 -0700

    fixing example files

--- example-files/sampleset_generator.py
+++ example-files/sampleset_generator.py
@@ -7,11 +7,15 @@ def generate_poisson_matrix(dimensions, lambd):
     matrix = np.random.poisson(lambd, shape)
     return matrix
 
-# Example usage
-dimensions = (10, 10, 10)  # dimensions
-lambd = 5  # lambda parameter
+# # Example usage
+# dimensions = (10, 10, 10)  # dimensions
+# lambd = 5  # lambda parameter
+# matrix = generate_poisson_matrix(dimensions, lambd)
+
+# two dimensional example! OK to reduce using principal components..
+dimensions = (1000, 1000)
+lambd = 5
 matrix = generate_poisson_matrix(dimensions, lambd)
-# print(matrix)
 
 # output to csv using pandas
 np.save('example-files/poisson_matrix.npy', matrix)
