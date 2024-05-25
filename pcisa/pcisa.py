35ae8ba834997efebb8805da677543d6e9ee2765
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-24 22:31:46 -0700

    fixing internal anndata to dataframe conversion

--- pcisa/pcisa.py
+++ pcisa/pcisa.py
@@ -31,7 +31,10 @@ def run_pca(data: str, n_pcs: int, output: str = None):
     # Load data and perform preprocessing
     # TODO: add try and catch for filetype not readable by anndata (and output error message)
     adata = anndata.read_h5ad(data)
-    df = adata.X.to_df()
+    df = pd.DataFrame(data=adata.X,
+                      columns=adata.var_names,
+                      index=adata.obs_names
+                    )
     
     ## TODO: add preprocessing based on user input
 
