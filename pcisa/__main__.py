1b06a07577477bccfaff9b4556bc0b82af888e37
Isabella Garcia <i4garcia@ucsd.edu>
2024-05-24 17:33:47 -0700

    moving main func for testing

--- pcisa/__main__.py
+++ pcisa/__main__.py
@@ -1,18 +1,18 @@
 # pcisa/__main__.py
 
-import argparse
-from .pcisa import run_pca
+# import argparse
+# from .pcisa import run_pca
 
-def main():
-    parser = argparse.ArgumentParser(description='Principal Component Analysis in Python')
-    parser.add_argument('--data', type=str, required=True, help='Path to input data file, expects matrix-like')
-    parser.add_argument('--n_pcs', type=int, required=True, help='Number of principal components calculated')
+# def main():
+#     parser = argparse.ArgumentParser(description='Principal Component Analysis in Python')
+#     parser.add_argument('--data', type=str, required=True, help='Path to input data file, expects matrix-like')
+#     parser.add_argument('--n_pcs', type=int, required=True, help='Number of principal components calculated')
 
-    parser.add_argument('--standardize', action='store_true', help='Standardize the input data before running PCA')
-    parser.add_argument('--output', type=str, help='Path to output file, .csv format default')
-    parser.add_argument('--plot', action='store_true', help='Plot the PCA results')
-    args = parser.parse_args()
+#     parser.add_argument('--standardize', action='store_true', help='Standardize the input data before running PCA')
+#     parser.add_argument('--output', type=str, help='Path to output file, .csv format default')
+#     parser.add_argument('--plot', action='store_true', help='Plot the PCA results')
+#     args = parser.parse_args()
 
-    print(f'Running PCA on {args.input} and saving results to {args.output}')
+#     print(f'Running PCA on {args.input} and saving results to {args.output}')
 
-    run_pca(args.input, args.output)
+#     run_pca(args.input, args.output)
--- pcisa/pcisa.py
+++ pcisa/pcisa.py
@@ -6,6 +6,23 @@ import numpy as np
 import anndata
 import pandas as pd
 
+import argparse
+from .pcisa import run_pca
+
+def main():
+    parser = argparse.ArgumentParser(description='Principal Component Analysis in Python')
+    parser.add_argument('--data', type=str, required=True, help='Path to input data file, expects matrix-like')
+    parser.add_argument('--n_pcs', type=int, required=True, help='Number of principal components calculated')
+
+    parser.add_argument('--standardize', action='store_true', help='Standardize the input data before running PCA')
+    parser.add_argument('--output', type=str, help='Path to output file, .csv format default')
+    parser.add_argument('--plot', action='store_true', help='Plot the PCA results')
+    args = parser.parse_args()
+
+    print(f'Running PCA on {args.input} and saving results to {args.output}')
+
+    run_pca(args.input, args.output)
+
 def run_pca(data: str, n_pcs: int, output: str = None):
     ## TODO: add proper documentation
     # Load data and perform preprocessing
