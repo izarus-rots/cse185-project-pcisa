fixing off-by-one indexing error in data cleanup section

pcisa/pcisa.py
@@ -158,10 +158,10 @@ def pca_calculation(data: pd.DataFrame, n_pcs: int):
 
     # remove extraneous information from datapoints
     for i in range(n_pcs):
-        pcadf[f'PC{i}'] = pcadf[f'PC{i}'].apply(lambda x: x.replace('(', '').replace(')', '').replace('+0j', ''))
+        pcadf[f'PC{i}'] = pcadf[f'PC{i+1}'].apply(lambda x: x.replace('(', '').replace(')', '').replace('+0j', ''))
     
     # recast to dtype=np.float64
     for i in range(n_pcs):
-        pcadf[f'PC{i}'] = pcadf[f'PC{i}'].astype(np.float64)
+        pcadf[f'PC{i}'] = pcadf[f'PC{i+1}'].astype(np.float64)
 
     return pcadf