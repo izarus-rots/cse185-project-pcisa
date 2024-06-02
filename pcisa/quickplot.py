5a0a3999bbab031170718e33dc61ab41740bfa00
Isabella Garcia <i4garcia@ucsd.edu>
2024-06-02 15:19:47 -0700

    removing leftover testing code from quickplot.py

--- pcisa/quickplot.py
+++ pcisa/quickplot.py
@@ -3,12 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Plot the data
-plt.figure(figsize=(10, 10))
-sns.scatterplot(x='PC1', y='PC2', data=df)
-plt.title('PCA')
-plt.savefig('PCA plot')
-
 def quickplot(csvpath: str, pcs : list, name : str = None):
     # ASSUMES pcs IS GIVEN AS AN INTEGER LIST! TODO: ADD CHECK!
     df = pd.read_csv(csvpath)
