fixing off by one and adding printout line for testing

pcisa/pcisa.py
@@ -155,14 +155,15 @@ def pca_calculation(data: pd.DataFrame, n_pcs: int):
     # cleaning up dataframe by renaming columns, removing extraneous rows, etc...
     pcdict = {}
     for i in range(n_pcs):
-        pcdict[i+1] = f'PC{i+1}'
+        pcdict[i] = f'PC{i+1}'
     # skip first row, first column
     pcadf = pcadf.iloc[1:, 1:]
     pcadf.rename(columns=pcdict)
+    print('COLS: ' + str(pcadf.columns))
 
     # remove extraneous information from datapoints
     for i in range(n_pcs):
-        pcadf[f'PC{i}'] = pcadf[f'PC{i+1}'].apply(lambda x: str(x).replace('(', '').replace(')', '').replace('+0j', ''))
+        pcadf[f'PC{i + 1}'] = pcadf[f'PC{i+1}'].apply(lambda x: str(x).replace('(', '').replace(')', '').replace('+0j', ''))
     
     # recast to dtype=np.float64
     for i in range(n_pcs):