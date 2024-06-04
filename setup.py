3f69d3134f3c5c73ca6acb75e43cd640c0f10de7
Isabella Garcia <i4garcia@ucsd.edu>
2024-06-03 18:14:10 -0700

    adding back version tracking (troubleshooting)

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ from setuptools import setup, find_packages
 VERSION = '0.1dev' # TODO: implement version tracking
 
 
-# # version-keeping code based on pybedtools & gymreklab demo project (https://github.com/gymreklab/cse185-demo-project/tree/main)
-# curdir = os.path.abspath(os.path.dirname(__file__))
-# MAJ = 0
-# MIN = 0
-# REV = 0
-# VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
-# with open(os.path.join(curdir, 'pcisa/version.py'), 'w') as fout:
-#         fout.write(
-#             "\n".join(["",
-#                        "# THIS FILE IS GENERATED FROM SETUP.PY",
-#                        "version = '{version}'",
-#                        "__version__ = version"]).format(version=VERSION)
-#         )
+# version-keeping code based on pybedtools & gymreklab demo project (https://github.com/gymreklab/cse185-demo-project/tree/main)
+curdir = os.path.abspath(os.path.dirname(__file__))
+MAJ = 0
+MIN = 0
+REV = 0
+VERSION = '%d.%d.%d' % (MAJ, MIN, REV)
+with open(os.path.join(curdir, 'pcisa/version.py'), 'w') as fout:
+        fout.write(
+            "\n".join(["",
+                       "# THIS FILE IS GENERATED FROM SETUP.PY",
+                       "version = '{version}'",
+                       "__version__ = version"]).format(version=VERSION)
+        )
 
 setup(
     name='pcisa',
